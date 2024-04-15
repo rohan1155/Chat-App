f666bcb3f9272cd0fe52edc68d96716552d5980d
Rohan <rohanchaudhry151@gmail.com>
2024-04-15 20:43:20 +0530

    deployment changes

--- client/components/Welcome.jsx
+++ client/components/Welcome.jsx
@@ -1,7 +1,7 @@
 export default function Welcome() {
   return (
     <div className="welcome-container">
-      <img src="../images/logo.png" alt="logo" className="logo" />
+      {/* <img src="../images/logo.png" alt="logo" className="logo" /> */}
       <p className="welcome-text">Welcome to Chat App!</p>
     </div>
   );
