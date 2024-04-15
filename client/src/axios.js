77b7531d22fe10b8cd103d669310f340a17ff4ae
Rohan <rohanchaudhry151@gmail.com>
2024-04-15 20:20:15 +0530

    deployment changes

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://chat-app-server-beryl-sigma.vercel.app/",
 });
 
 export default instance;
